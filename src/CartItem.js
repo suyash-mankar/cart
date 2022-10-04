5317a3c4fa9b87b79887ce7e18ddd41bf7245114
suyash-mankar <suyashmankar@gmail.com>
2022-10-04 19:15:21 +0530

    add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,11 @@ class CartItem extends React.Component {
     }
 
 
+    increaseQuantity = () => {
+        console.log('this', this);
+    }
+
+
     render(){
 
         const {price, title, qty} = this.state;
@@ -20,18 +25,34 @@ class CartItem extends React.Component {
         return (
 
             <div className="cart-item">
+
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
+
                 <div className="right-block">
                     <div style={ { fontSize: 25} }> {title} </div>
                     <div style={ { color: '#777'} }> Rs. {price} </div>
                     <div style={ { color: '#777'} }> Qty: {qty} </div>
+
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt='increase' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/3303/3303893.png' />
-                        <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992683.png' />
-                        <img alt='delete' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/484/484662.png' />
+                        <img 
+                        alt='increase' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/3303/3303893.png'
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt='decrease' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/992/992683.png' 
+                        />
+                        <img 
+                        alt='delete' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/484/484662.png' 
+                        />
 
                     </div>
                 </div>
